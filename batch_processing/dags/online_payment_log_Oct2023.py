2673b6e366e258bae00ee1223fb675e676f41111
risw24 <riswanda.alifarahman@gmail.com>
2023-12-13 11:12:14 +0700

    add workflow for load gcs to bq

--- batch_processing/dags/online_payment_log_Oct2023.py
+++ batch_processing/dags/online_payment_log_Oct2023.py
@@ -1,42 +1,35 @@
 import os
 
 from airflow import models
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator,BigQueryInsertJobOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
-
-DATASET_NAME = os.environ.get("GCP_DATASET_NAME", 'online_payment_log_Oct2023')
-FRAUD_TABLE_NAME = os.environ.get("GCP_TABLE_NAME", 'fraud_transaction')
-TRANSACTION_TABLE_NAME = os.environ.get("GCP_TABLE_NAME", 'transaction')
 
 dag = models.DAG(
     dag_id='online_payment_log',
     schedule_interval="@monthly",
-    start_date=datetime(2023, 10, 7),
+    start_date=datetime(2023, 10, 1),
     tags=['Oct2023'],
 )
-'''
-load_fraud_json_gcs_to_bq = GCSToBigQueryOperator(
-                    task_id="load_fraud_json_gcs_to_bq",
-                    bucket="df-11-group-1",
-                    source_objects=["fraud_confirmation/*.json"],
-                    source_format="NEWLINE_DELIMITED_JSON",
-                    destination_project_dataset_table=f"{DATASET_NAME}.{FRAUD_TABLE_NAME}",
-                    write_disposition="WRITE_TRUNCATE",
-                    dag=dag
-                )
-'''
+load_transaction = SimpleHttpOperator(
+                        task_id="load_transaction_gcs_to_bq",
+                        http_conn_id='load_transaction',
+                        method="GET",
+                        dag=dag
+                    )
+load_email = SimpleHttpOperator(
+                        task_id="load_email_gcs_to_bq",
+                        http_conn_id='load_email',
+                        method="GET",
+                        dag=dag
+                    )
 
-load_transaction_json_gcs_to_bq = GCSToBigQueryOperator(
-                    task_id="load_transaction_json_gcs_to_bq",
-                    bucket="df-11-group-1",
-                    source_objects=["transaction_1/*.json"],
-                    source_format="NEWLINE_DELIMITED_JSON",
-                    destination_project_dataset_table=f"{DATASET_NAME}.{TRANSACTION_TABLE_NAME}",
-                    write_disposition="WRITE_TRUNCATE",
-                    dag=dag
-                )
+load_fraud = SimpleHttpOperator(
+                        task_id="load_fraud_gcs_to_bq",
+                        http_conn_id='load_fraud',
+                        method="GET",
+                        dag=dag
+                    )
 
-#load_fraud_json_gcs_to_bq
-load_transaction_json_gcs_to_bq
+load_transaction >> load_email >> load_fraud
